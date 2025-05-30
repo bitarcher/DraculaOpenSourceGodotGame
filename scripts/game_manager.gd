a58c587aa88929fd753b9fe9278b22f79bdebf28
Strasser Michel <michel.strasser@free.fr>
2025-05-30 10:13:00 +0200

    Je travaill sur l'immunité

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,11 +3,19 @@ extends Node
 @export var num_of_lives: int
 var num_of_injuries_allowed 
 
+var immunity: bool = false
+
 signal player_injured() 
+
+var immunity_lost_timer: Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	reset_counters()
+	immunity_lost_timer = Timer.new()
+	add_child(immunity_lost_timer)
+	immunity_lost_timer.wait_time = 2.0
+	immunity_lost_timer.connect("timeout", _on_immunity_lost_timer_timeout)
 
 const INITIAL_NUM_OF_LIVES = 5
 const INITIAL_NUM_OF_INJURIES_ALLOWED = 3
@@ -21,11 +29,18 @@ func reset_counters():
 	num_of_injuries_allowed = INITIAL_NUM_OF_INJURIES_ALLOWED
 	
 func injured():
+	if(immunity):
+		return
+		
 	num_of_injuries_allowed -= 1
 	
 	if(num_of_injuries_allowed <= 0):
 		killed()
 	else:
+		print("start of immunity")
+		immunity = true
+		immunity_lost_timer.start()
+		
 		emit_signal("player_injured")
 		
 func killed():
@@ -41,3 +56,9 @@ func killed():
 func game_over():
 	print("game over")
 	#TODO
+
+
+func _on_immunity_lost_timer_timeout() -> void:
+	print("end of immunity")
+	immunity = false
+	immunity_lost_timer.stop()
