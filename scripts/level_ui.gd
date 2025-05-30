b22f85a308078ab2b1351f166be298cc31b71970
Strasser Michel <michel.strasser@free.fr>
2025-05-30 19:00:39 +0200

    ui compteur

---
+++ scripts/level_ui.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$Panel/GridContainer/MarginContainer2/CoinsLabel.text = str(GameManagerSingleton.num_of_coins)
+	$Panel/GridContainer/MarginContainer4/LivesLabel.text = str(GameManagerSingleton.num_of_lives)
