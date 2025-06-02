65999462e576cefad305b4fba67ef038f0a8679c
Strasser Michel <michel.strasser@free.fr>
2025-06-02 21:11:17 +0200

    level (5)

--- scripts/level_time.gd
+++ scripts/level_time.gd
@@ -5,5 +5,5 @@ func _process(_delta: float) -> void:
 	var time_ticks = Time.get_ticks_msec()
 	var time_diff = time_ticks - GameManagerSingleton.level_start_ticks
 	var time_diff_in_sec = time_diff / 1000.0
-	var time_diff_in_sec_str = "%.1f" % time_diff_in_sec
-	$Panel/Label.text = time_diff_in_sec_str + "sec."
+	var time_diff_in_sec_str_rounded_to_unit = "%d" % round(time_diff_in_sec)
+	$Panel/Label.text = time_diff_in_sec_str_rounded_to_unit + " sec."
