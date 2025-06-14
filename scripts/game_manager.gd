fff1b5d18e6c9f3f13a43da8abd7546789b206ea
Strasser Michel <michel.strasser@free.fr>
2025-06-14 22:57:04 +0200

    niv 14 nettoyé

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -28,7 +28,7 @@ var _menu_resource: Resource
 var _highscore_resource: Resource
 var _credits_resource: Resource
 
-const NUM_OF_LEVELS: int = 14
+const NUM_OF_LEVELS: int = 15
 
 func _init_level_resources():
 	_levels_resources = [
@@ -45,7 +45,8 @@ func _init_level_resources():
 		preload("res://scenes/p_level_10.tscn"),
 		preload("res://scenes/p_level_11.tscn"),
 		preload("res://scenes/p_level_12.tscn"),
-		preload("res://scenes/p_level_13.tscn")
+		preload("res://scenes/p_level_13.tscn"),
+		preload("res://scenes/p_level_14.tscn")
 	]
 	print(str(_levels_resources.size()) + " levels loaded")
 	
