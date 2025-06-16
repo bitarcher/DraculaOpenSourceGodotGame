b3c5d71513243a5729be3155a49c2aacf576274b
Strasser Michel <michel.strasser@free.fr>
2025-06-16 15:28:33 +0200

    wip inventory ui

--- inventory_and_recipes/user_interface/ui_root/ui_root.gd
+++ inventory_and_recipes/user_interface/ui_root/ui_root.gd
@@ -2,12 +2,13 @@ extends CanvasLayer
 
 @export var all_recipes:ResourceGroup
 
-@onready var player:Player = %Player
+var player:PlayerPlatformer
 @onready var inventory_dialog:InventoryDialog = %InventoryDialog
 @onready var crafting_dialog:CraftingDialog = %CraftingDialog
 var _all_recipes:Array[Recipe] = []
 
 func _ready():
+	player = ToolsSingleton.get_player_platformer()
 	all_recipes.load_all_into(_all_recipes)
 
 func _unhandled_input(event):
@@ -16,7 +17,3 @@ func _unhandled_input(event):
 		
 	if event.is_action_released("crafting"):
 		crafting_dialog.open(_all_recipes, player.inventory)
-
-
-
-
