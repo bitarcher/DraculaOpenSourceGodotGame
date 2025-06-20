a8eebcd62a7c92e41b3841999de84326ec59ff21
Strasser Michel <michel.strasser@free.fr>
2025-06-20 17:25:10 +0200

    ui improvements for recipes

--- inventory_and_recipes/user_interface/item_slot/item_slot.gd
+++ inventory_and_recipes/user_interface/item_slot/item_slot.gd
@@ -1,7 +1,9 @@
 extends PanelContainer
 
 @onready var _texture_rect:TextureRect = %TextureRect
+@onready var label: Label = %Label
 
 func display(item:Item):
 	_texture_rect.texture = item.icon
+	label.text = item.name
 	
