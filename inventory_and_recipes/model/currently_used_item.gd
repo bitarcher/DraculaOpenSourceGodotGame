e47e486465cb766c199c9ebdbeba31dda991bca9
Strasser Michel <michel.strasser@free.fr>
2025-06-22 10:26:27 +0200

    working on currently used items

--- inventory_and_recipes/model/currently_used_item.gd
+++ inventory_and_recipes/model/currently_used_item.gd
@@ -12,7 +12,15 @@ func _init() -> void:
 static func new_from_item(item: Item) -> CurrentlyUsedItem:
 	var result: CurrentlyUsedItem = CurrentlyUsedItem.new()
 	result.item =item
-	result.start_ticks = Time.get_ticks_usec()
+	result.start_ticks = Time.get_ticks_usec() * 1.0
 	result.planned_end_ticks = result.start_ticks + item.duration_in_seconds * 1000.0
+	
+	return result
+	
+func get_usage_percent() -> float:
+	var current_ticks = Time.get_ticks_msec() * 1.0
+	var total_duration_ticks = planned_end_ticks - start_ticks
+	var used_ticks = current_ticks - start_ticks
+	var result = used_ticks / total_duration_ticks
 	
 	return result
