05e52cbf4675005fc9817bbe447d941a2334d64c
Strasser Michel <michel.strasser@free.fr>
2025-06-23 16:13:02 +0200

    bat works but I'm not satisfied of direction chosing

---
+++ scripts/persist/bat_saved_data.gd
@@ -0,0 +1,6 @@
+class_name BatSavedData
+extends SavedData
+
+@export var go_left_once_awaken: bool = true
+@export var awake: bool = false
+@export var direction_vector:Vector2 = Vector2.ONE
---
+++ scripts/persist/bat_serializable_component.gd
@@ -0,0 +1,36 @@
+class_name BatSerializableComponent
+extends ASerializableComponent
+
+func _get_bat() -> Bat:
+	return get_parent()
+	
+func on_save_game(saved_datas):
+	var bat : Bat = _get_bat()
+	var my_data = BatSavedData.new()
+	my_data.scene_path = "res://scenes/bat.tscn"
+	my_data.position = bat.global_position
+	my_data.direction_vector.x  = bat._direction_vector.x
+	my_data.direction_vector.y  = bat._direction_vector.y
+	my_data.awake = bat.awake
+	my_data.go_left_once_awaken = bat.go_left_once_awaken
+	
+	saved_datas.append(my_data)
+	
+func on_before_load_game():
+	var slime_enemy = _get_bat()
+	slime_enemy.get_parent().remove_child(slime_enemy)
+	slime_enemy.queue_free()
+
+func on_load_game(saved_data):
+	var my_saved_data = saved_data as SavedData
+	var bat : Bat  = _get_bat()
+	bat.global_position = my_saved_data.position
+	
+	if(my_saved_data is BatSavedData):
+		var my_data = my_saved_data as BatSavedData
+		
+		bat._direction_vector.x = my_data.direction_vector.x
+		bat._direction_vector.y = my_data.direction_vector.y
+		bat.awake = my_data.awake
+		bat.go_left_once_awaken = my_data.go_left_once_awaken
+	
