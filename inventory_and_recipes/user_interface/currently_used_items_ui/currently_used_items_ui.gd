3c2f22b2ee6072c96a88f28ccf5f2e13031a6622
Strasser Michel <michel.strasser@free.fr>
2025-06-23 09:56:06 +0200

    working on currently used item ui, now displayed, but problem with progress bar

--- inventory_and_recipes/user_interface/currently_used_items_ui/currently_used_items_ui.gd
+++ inventory_and_recipes/user_interface/currently_used_items_ui/currently_used_items_ui.gd
@@ -1,9 +1,13 @@
-class_name CurrentlyUsedIemsUi
+# currently_used_items_ui.gd
+class_name CurrentlyUsedIemsUi # <-- Attention à la faute de frappe "Iems" au lieu de "Items" si ce n'est pas intentionnel
 extends Panel
 
 @onready var v_box_container: VBoxContainer = $VBoxContainer
 
-# Called when the node enters the scene tree for the first time.
+# Chargez votre scène CurrentlyUsedItemUi comme une PackedScene
+# Assurez-vous que le chemin est correct !
+const CURRENTLY_USED_ITEM_UI_SCENE = preload("res://inventory_and_recipes/user_interface/currently_used_item_ui/currently_used_item_ui.tscn")
+
 func _ready() -> void:
 	GameManagerSingleton.currently_used_items.cu_items_changed.connect(_cu_items_changed)
 	_recreate_content()
@@ -19,6 +23,12 @@ func _recreate_content():
 	var currently_used_items = GameManagerSingleton.currently_used_items
 	
 	for currently_used_item in currently_used_items.get_cu_items():
-		var currently_used_item_ui = CurrentlyUsedItemUi.new()
+		# C'est la ligne clé qui change !
+		var currently_used_item_ui_instance = CURRENTLY_USED_ITEM_UI_SCENE.instantiate()
+		
+		# Assurez-vous de caster l'instance si vous utilisez le class_name pour le typage fort,
+		# ou simplement laissez-le comme un Node (CurrentlyUsedItemUi est son class_name)
+		var currently_used_item_ui: CurrentlyUsedItemUi = currently_used_item_ui_instance as CurrentlyUsedItemUi
+		
+		currently_used_item_ui.currently_used_item = currently_used_item # Ceci appelle le setter maintenant correctement
 		v_box_container.add_child(currently_used_item_ui)
-		currently_used_item_ui.currently_used_item = currently_used_item
