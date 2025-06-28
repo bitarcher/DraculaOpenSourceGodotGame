ca088cde7adade04afdc3800335cc4db85252ae5
Strasser Michel <michel.strasser@free.fr>
2025-06-28 14:25:32 +0200

    slime has now nice injury and killed effect, but sound is missing

--- scripts/slime_enemy.gd
+++ scripts/slime_enemy.gd
@@ -7,6 +7,8 @@ const SPEED = 10.0
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var blood_particles: GPUParticles2D = %BloodParticles
+var killed = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,6 +20,23 @@ func _process(delta: float) -> void:
 	if(ray_cast_left.is_colliding()):
 		direction = 1
 		animated_sprite.flip_h = false
-		
-	position.x += delta * SPEED * direction
 	
+	if not killed:
+		position.x += delta * SPEED * direction
+	
+
+
+func _on_damage_receiver_component_damage_received(strength: float, new_life_counter: float) -> void:
+	blood_particles.emitting = true
+
+
+func _on_damage_receiver_component_killed() -> void:
+	killed = true
+	animated_sprite.play("dying")
+	await animated_sprite.animation_finished
+
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 3.0)
+	await tween.finished
+
+	self.queue_free()
